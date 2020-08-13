0d0339d578729548d3b833fd3e8c833b871a68cd
coconut-palm <969606616@qq.com>
2020-08-13 08:22:19 +0800

    2020-8-13

---
+++ src/com/atguigu/service/UserService.java
@@ -0,0 +1,15 @@
+package com.atguigu.service;
+
+import com.atguigu.pojo.User;
+
+public interface UserService {
+
+    //注册用户
+    public void registUser(User user);
+
+    //登录
+    public User login(User user);
+
+    //检查用户名是否可用
+    public boolean existUsername(String username);
+}
